0ff0aad544ae9c433caa132dbc3ecec75a6d3f6c
Lukasz Malucha <lucasmalucha@gmail.com>
2022-03-12 19:24:57 +0000

    data

--- ToDo/src/app.js
+++ ToDo/src/app.js
@@ -1,6 +1,11 @@
 App = {
+  loading: false,
+  contracts: {},
   load: async () => {
     await App.loadWeb3()
+    await App.loadAccount()
+    await App.loadContract()
+    await App.render()
   },
   // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
   loadWeb3: async () => {
@@ -35,7 +40,47 @@ App = {
       }
     })
   },
+  loadAccount: async () => {
+    const account = await web3.eth.getAccounts();
+    App.account = await account[0];
+  },
+  loadContract: async () => {
+    const todoList  = await $.getJSON('TodoList.json')
+    App.contracts.TodoList = TruffleContract(todoList)
+    App.contracts.TodoList.setProvider(App.web3Provider)
 
+
+    App.todoList = await App.contracts.TodoList.deployed()
+
+  },
+
+  render: async () => {
+//  Prevent double render
+    if(App.loading) {
+      return
+    }
+    App.setLoading(true)
+
+
+//  Render Account
+    $('#account').html(App.account)
+
+//  Update loading state
+  App.setLoading(false)
+  },
+
+  setLoading: (boolean) => {
+    App.loading = boolean
+    const loader = $('#loader')
+    const content = $('#content')
+    if (boolean) {
+      loader.show()
+      content.hide()
+    } else {
+      loader.hide()
+      content.show()
+    }
+  },
 }
 
 
