87f9942193b60ea60b82a3f6b9955a5d6b7ce7e2
Lukasz Malucha <lucasmalucha@gmail.com>
2022-04-19 18:33:04 +0100

    inbox

---
+++ _Basics/Solidity_Contracts/Inbox.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+contract Inbox {
+    string public message;
+
+    // Function no longer can have the same name as contract - use 'constructor' keyword instead
+    constructor(string memory InitialMessage) {
+        message = InitialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+    // 'view' - we are not attempting to modify any data with that function
+    // 'returns' - only with view or constant
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+}
+
