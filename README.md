6485e489aa17c1665db75a9b22ca667077dbc5b2
Lukasz Malucha <lucasmalucha@gmail.com>
2022-04-24 20:45:20 +0100

    compile

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Django + Web3.py
+
+
+npm install --save mocha ganache-cli web3
--- _Basics/Boilerplate/compile.js
+++ _Basics/Boilerplate/compile.js
@@ -7,6 +7,6 @@ const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
 //// Compile Statement - 1 stands for number of contracts to compile
-console.log(solc.compile(source, 1));
+module.exports = solc.compile(source, 1).contracts;
 
 
