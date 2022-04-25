3c641157c705cbc5bcbe6b72af6d20fd7325aa3b
Lukasz Malucha <lucasmalucha@gmail.com>
2022-04-25 14:12:49 +0100

    test

--- _Basics/Boilerplate/test/Inbox.test.js
+++ _Basics/Boilerplate/test/Inbox.test.js
@@ -1,9 +1,49 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
+// Constructor
 const Web3 = require('web3');
+// Instance
+const web3 = new Web3(ganache.provider());
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class Car {
+//  park() {
+//    return 'stopped';
+//  }
+//
+//  drive() {
+//    return 'vroom';
+//  }
+//}
+//
+//// Initialize variable
+//let car;
+//
+//beforeEach(() => {
+//  car = new Car();
+//});
+//
+//describe("Car Class", () => {
+//  it('park function', () => {
+//    assert.equal(car.park(), 'stopped');
+//  });
+//});
+//
+//
 
 
 
