1b57d050f5cae4c8b86c9c6ba6ce124e06af0c39
Lukasz Malucha <lucasmalucha@gmail.com>
2022-05-26 21:52:33 +0100

    data

--- _Basics/Redux_Starter/src/index.js
+++ _Basics/Redux_Starter/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
-const YOUTUBE_KEY = process.env.VUE_APP_YOUTUBE_KEY
 
-const App = function() {
+
+
+const YOUTUBE_KEY = process.env.YOUTUBE_KEY
+
+YTSearch({key: YOUTUBE_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
+
+const App = () => {
   return (
     <div>
       <SearchBar />
