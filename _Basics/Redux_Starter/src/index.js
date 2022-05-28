41e3874e675195010aa7e6319e0d2c01727de47f
Lukasz Malucha <lucasmalucha@gmail.com>
2022-05-28 19:53:03 +0100

    list render

--- _Basics/Redux_Starter/src/index.js
+++ _Basics/Redux_Starter/src/index.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 
 
 
-const YOUTUBE_KEY = process.env.YOUTUBE_KEY
 
-YTSearch({key: YOUTUBE_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { videos: [] };
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    YTSearch({key: YOUTUBE_KEY, term: 'surfboards'}, (queryset) => {
+      this.setState({videos: queryset });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 
